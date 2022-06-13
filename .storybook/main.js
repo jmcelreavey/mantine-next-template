53859241e71c4c4f9328822c3833f567d0da6edf
jmcelreavey <j.mcelreavey@gmail.com>
2022-06-13 13:59:09 +0000

    ✨ Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,21 @@
 module.exports = {
-  stories: ['../**/*.story.mdx', '../**/*.story.@(js|jsx|ts|tsx)'],
-  addons: [
-    'storybook-dark-mode',
-    {
-      name: 'storybook-addon-turbo-build',
-      options: { optimizationLevel: 2 },
-    },
+  "stories": [
+    "../**/**/*.story.mdx",
+    "../**/**/*.story.@(js|jsx|ts|tsx)"
   ],
-  framework: '@storybook/react',
-};
+  "addons": [
+      'storybook-dark-mode',
+      {
+        name: 'storybook-addon-turbo-build',
+        options: { optimizationLevel: 2 },
+      },
+  
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
